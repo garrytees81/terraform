1d17af1aba6610094e2587dfe4dc9fd86aa302ee
garrytees81 <guriq.pannu@outlook.com>
2023-03-22 22:26:44 +0000

    webapp1

--- locals.tf
+++ locals.tf
@@ -3,3 +3,9 @@ locals {
   location = "northeurope"
   
 }
+
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
--- webapp.tf
+++ webapp.tf
@@ -67,7 +67,7 @@ resource "azurerm_windows_web_app" "webapp2" {
 
 
 resource "azurerm_app_service_source_control" "github1" {
-  app_id   = azurerm_windows_web_app.webapp1.id
+  app_id   = azurerm_linux_web_app.webapp1.id
   repo_url = "https://github.com/garrytees81/webapp1"
   branch   = "master"
   use_manual_integration = true
